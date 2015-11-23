390855cc23df825c4764f9e5dce70e8013ddba06
cz <598799959@qq.com>
2015-11-23 20:53:51 +0800

    add datetimepicker in vote

--- static/javascript/vote.js
+++ static/javascript/vote.js
@@ -1,4 +1,12 @@
+var sth=$;
 var vote_util={
+    optionHTML:'<a id="option<%=i%>" href="#" class="list-group-item">'+
+        '<div id="datetime<%=i%>" class="input-group date">'+
+              '<span class="input-group-addon"><%=i%>.</span>'+
+              '<input class="form-control" size="10" type="text" value="" readonly>'+
+              '<span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>'+
+			  '<span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>'+
+        '</div>'+'</a>',
     bind_group:function(){
         $("input.groupselect").click(function(e){
             var $label=$(e.target).closest("label");
@@ -11,9 +19,9 @@ var vote_util={
     },
     bind:function(){
         vote_util.bind_group();
-        $("")
+        $("#optionlist");
     }
 }
 $(document).ready(function(){
     vote_util.bind();
-})
+});

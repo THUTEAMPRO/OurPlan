642b068041d9f6c0fa6811f75018bb65710713dd
qiupengfei <643770083@qq.com>
2015-12-16 21:07:09 +0800

    conflict

---
+++ static/javascript/thing.js
@@ -0,0 +1,17 @@
+ 
+
+{“memeber”:[
+            {
+                  “name”:”Tom”,
+                  “age”:22,
+                  “country”:”USA”
+      },
+      {
+                  “name”:”WangMing”,
+                  “age”:25,
+                  “country”:”China”
+      }
+      ]
+      }
+
+var last=obj.toJSONString();
